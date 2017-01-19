34a7575076a11f89179f3a01da81a6f81b23ccb6
Michael Hsu <evenchange4@gmail.com>
2017-01-19 18:03:55 +0800

    feat(Small): new component

---
+++ packages/mcs-lite-ui/src/Small/Small.example.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { storiesOf } from '@kadira/storybook';
+import Small from '.';
+import P from '../P';
+
+storiesOf('Small', module)
+  .addWithInfo(
+    'API',
+    '縮小字型。',
+    () =>
+      <Small>
+        Small content
+      </Small>,
+    { inline: true },
+  )
+  .addWithInfo(
+    'Within <P>',
+    '段落中出現。',
+    () =>
+      <P>
+        Nisi eu eiusmod cupidatat aute laboris commodo excepteur
+        <Small>
+          &nbsp;Small content&nbsp;
+        </Small>
+        incididunt incididunt aliquip pariatur est minim officia sit.
+      </P>,
+    { inline: true, propTables: false },
+  )
+  .addWithInfo(
+    'With color',
+    '來自上層的顏色。',
+    () =>
+      <P color="primary">
+        Nisi eu eiusmod cupidatat aute laboris commodo excepteur
+        <Small>
+          &nbsp;Small content&nbsp;
+        </Small>
+        incididunt incididunt aliquip pariatur est minim officia sit.
+      </P>,
+    { inline: true, propTables: false },
+  );
---
+++ packages/mcs-lite-ui/src/Small/Small.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Small = styled.small`
+  font-size: ${props => props.theme.fontSize.small};
+`;
+
+Small.displayName = 'Small';
+
+export default Small;
