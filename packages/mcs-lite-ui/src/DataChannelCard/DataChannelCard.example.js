4bd2a6689edeb878c610db1033af2fa946e88f75
Michael Hsu <evenchange4@gmail.com>
2017-01-19 23:19:12 +0800

    feat(DataChannelCard): add Switch story

--- packages/mcs-lite-ui/src/DataChannelCard/DataChannelCard.example.js
+++ packages/mcs-lite-ui/src/DataChannelCard/DataChannelCard.example.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@kadira/storybook';
 import DataChannelCard from '.';
+import Switch from '../Switch';
 
 storiesOf('DataChannelCard', module)
   .addWithInfo(
@@ -13,6 +14,19 @@ storiesOf('DataChannelCard', module)
         description="You can input description of controller here. You can input description of You can input description of controller here. You can input description of"
       >
         Children
+      </DataChannelCard>,
+    { inline: true },
+  )
+  .addWithInfo(
+    'Switch data channel',
+    '套用 Switch component。',
+    () =>
+      <DataChannelCard
+        title="Title"
+        subtitle="Last data point time : 2015-06-12 12:00"
+        description="You can input description of controller here. You can input description of You can input description of controller here. You can input description of"
+      >
+        <Switch />
       </DataChannelCard>,
     { inline: true },
   );
