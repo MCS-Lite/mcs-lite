ae047e0027a285d0ed56a3991a90d78be81e444a
Michael Hsu <evenchange4@gmail.com>
2017-01-21 01:35:14 +0800

    feat(babel-preset): use styled-components plugin and latest

--- packages/babel-preset-mcs-lite/index.js
+++ packages/babel-preset-mcs-lite/index.js
@@ -1,16 +1,28 @@
 module.exports = {
   presets: [
     require.resolve('babel-preset-react'),
-    require.resolve('babel-preset-es2015'),
+    require.resolve('babel-preset-latest'),
     require.resolve('babel-preset-stage-0'),
   ],
   plugins: [
     require.resolve('babel-plugin-add-module-exports'),
+
+    // minify styled-components css
+    [
+      require.resolve('babel-plugin-styled-components'),
+      {
+        ssr: false,
+        displayName: false,
+        transpileTemplateLiterals: false,
+      },
+    ],
+
     [
       require.resolve('babel-plugin-lodash'),
       {
         id: [
           'ramda',
+          'mcs-lite-ui',
         ],
       },
     ],
