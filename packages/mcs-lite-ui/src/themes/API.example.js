8e573b4b4e0dbbc558e9485ceae57296c2c4a7d5
Michael Hsu <evenchange4@gmail.com>
2017-01-22 21:00:25 +0800

    fix(API): remove data property

--- packages/mcs-lite-ui/src/themes/API.example.js
+++ packages/mcs-lite-ui/src/themes/API.example.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@kadira/storybook';
 import { kindList, theme } from 'mcs-lite-theme';
 
-const JsonDisplay = data =>
+const JsonDisplay = ({ data }) =>
   <div><pre>{JSON.stringify(data, null, 2) }</pre></div>;
 
 storiesOf('API [mcs-lite-theme]', module)
