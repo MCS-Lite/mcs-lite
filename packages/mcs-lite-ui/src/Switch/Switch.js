e8ca12dab8c2ed6ae686014765971f250b5f082f
Michael Hsu <evenchange4@gmail.com>
2017-01-23 21:37:10 +0800

    fix(Switch): cross-browser support

--- packages/mcs-lite-ui/src/Switch/Switch.js
+++ packages/mcs-lite-ui/src/Switch/Switch.js
@@ -3,15 +3,13 @@ import styled from 'styled-components';
 
 const Switch = styled.div`
   position: relative;
-  display: flex;
   height: 50px;
   width: 82px;
   box-sizing: border-box;
-  background-color: ${props => props.checked ? props.theme.color.success : props.theme.color.grayBase};
   border-radius: 24px;
-  align-items: center;
   transition: background-color cubic-bezier(0.47, 0, 0.75, 0.72) 0.2s;
   cursor: pointer;
+  background-color: ${props => props.checked ? props.theme.color.success : props.theme.color.grayBase};
 
   &::after {
     content: " ";
@@ -22,6 +20,7 @@ const Switch = styled.div`
     background-color: ${props => props.theme.color.white};
     cursor: pointer;
     box-shadow: 0 3px 6px rgba(0, 0, 0, 0.17);
+    top: 3px;
     left: ${props => props.checked ? '32px' : 0};
     transition: left cubic-bezier(0.47, 0, 0.75, 0.72) 0.2s;
     margin: 0 3px;
