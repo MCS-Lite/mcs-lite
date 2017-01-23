81f0678da8f1d2e041a202bc887b842d915f6538
Michael Hsu <evenchange4@gmail.com>
2017-01-23 13:47:16 +0800

    feat(theme): new color opacity function (#19)

--- packages/mcs-lite-theme/src/index.js
+++ packages/mcs-lite-theme/src/index.js
@@ -1,9 +1,11 @@
 import theme from './theme';
 import kindList from './kindList';
 import darken from './darken';
+import opacity from './opacity';
 
 export default {
   theme,
   kindList,
   ...darken,
+  opacity,
 };
