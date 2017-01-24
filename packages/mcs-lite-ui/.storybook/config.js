727cd9d9886dcce8bff3957c837297183aa53355
Michael Hsu <evenchange4@gmail.com>
2017-01-24 15:35:47 +0800

    feat(Storybook): use storybook-addon-options to customize homepage

--- packages/mcs-lite-ui/.storybook/config.js
+++ packages/mcs-lite-ui/.storybook/config.js
@@ -1,11 +1,23 @@
 import { configure, addDecorator, setAddon } from '@kadira/storybook';
 import infoAddon from '@kadira/react-storybook-addon-info';
+import { setOptions } from '@kadira/storybook-addon-options';
 import centered from './decorator-centered';
 
 const context = require.context('../src/', true, /\.example\.js$/);
 
 setAddon(infoAddon);
 addDecorator(centered);
+
+setOptions({
+  name: 'MCS LITE UI',
+  url: '/',
+  goFullScreen: false,
+  showLeftPanel: true,
+  showDownPanel: false,
+  showSearchBox: false,
+  downPanelInRight: false,
+  sortStoriesByKind: true,
+});
 
 function loadStories() {
   context.keys().forEach(context);
