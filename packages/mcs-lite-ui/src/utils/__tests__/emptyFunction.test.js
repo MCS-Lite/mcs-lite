2131b3848adea15fe289b0ef1fd59104384cec0b
Michael Hsu <evenchange4@gmail.com>
2017-02-02 15:37:39 +0800

    test(ClickOutside): add event testing

--- packages/mcs-lite-ui/src/utils/__tests__/emptyFunction.test.js
+++ packages/mcs-lite-ui/src/utils/__tests__/emptyFunction.test.js
@@ -2,7 +2,7 @@ import emptyFunction from '../emptyFunction';
 
 describe('emptyFunction', () => {
   it('should return emptyFunction function.', () => {
-    expect(emptyFunction).toBeInstanceOf(Function);
+    expect(typeof emptyFunction).toBe('function');
   });
 
   it('should return undefined when invoked.', () => {
