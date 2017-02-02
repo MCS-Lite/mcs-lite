a5bada1141d6eab2c05657fb6083eb0f13c9a5b5
Michael Hsu <evenchange4@gmail.com>
2017-02-02 17:15:50 +0800

    mcs-lite-theme init

--- packages/mcs-lite-mobile-web/src/App.js
+++ packages/mcs-lite-mobile-web/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+import { Button, DataChannelCard, DataChannel } from 'mcs-lite-ui';
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,6 +11,7 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
+          <Button>Button</Button>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
