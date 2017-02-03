c4a76c065aaad3418b697b38e28a948019212c03
Michael Hsu <evenchange4@gmail.com>
2017-02-04 00:43:11 +0800

    init

---
+++ packages/mcs-lite-ui/src/PullToRefresh/PullToRefresh.js
@@ -0,0 +1,83 @@
+import React, { PropTypes } from 'react';
+import styled from 'styled-components';
+import { IconTrashO, IconEllipsisV } from 'mcs-lite-icon';
+
+const height = 60;
+
+const PullWrapper = styled.div`
+  position: relative;
+  height: 100%;
+  overflow-y: hidden;
+  background-color: ${props => props.theme.color.grayBase};
+`;
+
+const LoadingContainer = styled.div`
+  position: absolute;
+  width: 100%;
+  height: ${height}px;
+  transform: translate3d(0 , ${props => (Number(props.distance) - height)}px, 0);
+  transition: all .25s ease;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: green;
+
+  > * {
+    transition: all .25s ease;
+    transform: rotate(${props => props.distance >= 60 ? 0 : -180}deg);
+  }
+`;
+
+const ChildrenContainer = styled.div`
+  position: relative;
+  transform: translate3d(0 , ${props => props.distance}px, 0);
+  transition: all .25s ease;
+  background-color: red;
+  height: 100%;
+`;
+
+class PullToRefresh extends React.Component {
+  state = { distance: 0, isRefreshing: false };
+  onTouchStart = (e) => {
+    this.from = e.touches[0].pageY;
+    this.to = e.touches[0].pageY;
+  }
+  onTouchMove = (e) => {
+    this.to = e.touches[0].pageY;
+    const distance = this.to - this.from;
+    if (distance < 0) return;   // disable pulling up
+    if (distance > 120) return; // limit range of pulling down
+    if (this.state.isRefreshing) return;
+
+    this.setState({ distance: this.to - this.from });
+  }
+  onTouchEnd = () => {
+    if (this.state.distance < height || this.state.isRefreshing) return;
+
+    this.setState({ distance: height, isRefreshing: true });
+    this.props.onRefresh(this.doneCallback);
+  }
+
+  doneCallback = () => this.setState({ distance: 0, isRefreshing: false });
+  render() {
+    const { children, ...otherProps } = this.props;
+
+    return (
+      <PullWrapper
+        {...otherProps}
+        onTouchStart={this.onTouchStart}
+        onTouchEnd={this.onTouchEnd}
+        onTouchMove={this.onTouchMove}
+      >
+        <LoadingContainer distance={this.state.distance}>
+          {this.state.isRefreshing ? <IconEllipsisV /> : <IconTrashO />}
+        </LoadingContainer>
+        <ChildrenContainer distance={this.state.distance}>
+          {children}
+        </ChildrenContainer>
+      </PullWrapper>
+    );
+  }
+}
+
+export default PullToRefresh;
