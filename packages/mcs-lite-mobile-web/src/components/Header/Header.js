8e40b3348a55027d671bad16a2f1f18ba9b59600
Michael Hsu <evenchange4@gmail.com>
2017-02-03 11:38:14 +0800

    feat(react-router): add navigation (#54)

--- packages/mcs-lite-mobile-web/src/components/Header/Header.js
+++ packages/mcs-lite-mobile-web/src/components/Header/Header.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router';
+
+const StyledLink = styled(Link)`
+  color: blue;
+  margin-right: 5px;
+`;
 
 const Header = () =>
   <header>
-    Header
+    <StyledLink to="/">Homepage</StyledLink>
+    <StyledLink to="/devices">Devices</StyledLink>
   </header>;
 
 export default Header;
