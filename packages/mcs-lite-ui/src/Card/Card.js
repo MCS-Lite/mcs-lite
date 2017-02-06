2b9d92e40bb214b7e7f0f3b28d4ae6d1d0c6f73b
Michael Hsu <evenchange4@gmail.com>
2017-02-06 14:21:52 +0800

    fix(Card): overflow border-radius

--- packages/mcs-lite-ui/src/Card/Card.js
+++ packages/mcs-lite-ui/src/Card/Card.js
@@ -6,6 +6,7 @@ const Card = styled.div`
   box-shadow: 0 1px 5px 0 rgba(0, 0, 0, 0.2);
   border-radius: 3px;
   background-color: ${R.path(['theme', 'color', 'white'])};
+  overflow: hidden;
 `;
 
 Card.displayName = 'Card';
