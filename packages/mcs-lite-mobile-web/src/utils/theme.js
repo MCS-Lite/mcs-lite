ed61c056d2cbd4aedcfbc76ab541835c7b60ff01
Michael Hsu <evenchange4@gmail.com>
2017-02-07 18:04:19 +0800

    update theme

--- packages/mcs-lite-mobile-web/src/utils/theme.js
+++ packages/mcs-lite-mobile-web/src/utils/theme.js
@@ -12,10 +12,10 @@ const mobileTheme = {
     p: '1rem',
     h6: '1rem',
     h5: '1rem',     // = 14px;
-    h4: '1.14rem',
-    h3: '1.14rem',  // = 16px;
-    h2: '1.43rem',
-    h1: '1.43rem',  // = 20px;
+    h4: '1.14rem',  // = 16px;
+    h3: '1.43rem',  // = 20px;
+    h2: '1.57rem',  // = 22px;
+    h1: '2.29rem',  // = 32px;
   },
 };
 
