28561c979169d33f5d2cc9aa651429aa8eb06eb2
Michael Hsu <evenchange4@gmail.com>
2017-02-09 10:26:41 +0800

    feat(StyledLink): style Link of react-router

---
+++ packages/mcs-lite-mobile-web/src/components/StyledLink/StyledLink.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { Link } from 'react-router';
+
+const StyledLink = styled(Link)`
+  display: block;
+  text-decoration: none;
+  cursor: pointer;
+`;
+
+export default StyledLink;
