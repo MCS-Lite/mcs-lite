ebdf6694788b28f7235595f7ddff3025126fcc04
Michael Hsu <evenchange4@gmail.com>
2017-02-09 09:33:04 +0800

    fix(modules): remove useless ui modules

--- packages/mcs-lite-mobile-web/src/modules/index.js
+++ packages/mcs-lite-mobile-web/src/modules/index.js
@@ -3,17 +3,14 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import devices, { epics as devicesEpics } from './devices';
 import auth, { epics as authEpics } from './auth';
-import ui, { epics as uiEpics } from './ui';
 
 export const epic = combineEpics(
   ...devicesEpics,
   ...authEpics,
-  ...uiEpics,
 );
 
 export const reducer = combineReducers({
   devices,
   auth,
-  ui,
   routing: routerReducer,
 });
