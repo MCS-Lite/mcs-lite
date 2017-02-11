8cd8c36d51cedff32d574a7e202242104e6815a3
Michael Hsu <evenchange4@gmail.com>
2017-02-11 17:32:35 +0800

    chore(storyshot): mocking info-addon (#98)

--- packages/mcs-lite-ui/.storybook/storyshots-setup.js
+++ packages/mcs-lite-ui/.storybook/storyshots-setup.js
@@ -1,10 +1,13 @@
 import { addDecorator, setAddon } from '@kadira/storybook';
 import centered from './decorator-centered';
 
-// 避免 storyshot 會拍到 InfoAddon 附加的結構。
+/**
+ * Mocking InfoAddon for preventing big snapshots.
+ * ref: https://github.com/storybooks/storyshots/issues/78#issuecomment-278123759
+ */
 const mockInfoAddon = {
   addWithInfo(storyName, info, storyFn) {
-    this.add(storyName, context => storyFn(context));
+    return this.add(storyName, context => storyFn(context));
   },
 };
 
