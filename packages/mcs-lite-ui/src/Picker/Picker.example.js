919dfb69515abe5b0e2e08d0321859cb3bde2128
Michael Hsu <evenchange4@gmail.com>
2017-02-17 22:31:19 +0800

    add more items

--- packages/mcs-lite-ui/src/Picker/Picker.example.js
+++ packages/mcs-lite-ui/src/Picker/Picker.example.js
@@ -12,7 +12,11 @@ class PickerWithState extends React.Component {
         <Picker
           value={this.state.value1}
           onChange={this.onValue1Change}
-          labels={['name 1', 'name 2', 'name 3', 'name 4']}
+          labels={[
+            'name 1', 'name 2', 'name 3', 'name 4',
+            'name 5', 'name 6', 'name 7', 'name 8',
+            'name 9', 'name 10', 'name 11', 'name 12',
+          ]}
         />
 
         <Picker
