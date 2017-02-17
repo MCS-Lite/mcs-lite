1911f3318a5a0144236438302d9e7a4b5b3b9793
Michael Hsu <evenchange4@gmail.com>
2017-02-17 14:08:53 +0800

    feat(Toast): add new component

---
+++ packages/mcs-lite-ui/src/Toast/Toast.js
@@ -0,0 +1,36 @@
+import { PropTypes } from 'react';
+import styled from 'styled-components';
+import { opacity } from 'mcs-lite-theme';
+import R from 'ramda';
+import Card from '../Card';
+
+const lighten = opacity(0.1);
+
+const mapColorByKind = props => R.pipe(
+  R.path(['kind']),
+  R.cond([
+    [R.equals('default'), R.always(props.theme.color.grayBase)],
+    [R.T, R.always(props.theme.color[props.kind])],
+  ]),
+);
+
+const Toast = styled(Card)`
+  display: flex;
+  align-items: center;
+  padding: 0 16px;
+  min-height: ${props => props.theme.base.inputHeight};
+  color: ${mapColorByKind};
+  border: 1px solid ${mapColorByKind};
+  background-color: ${props => lighten(props.theme.color[props.kind])};
+`;
+
+Toast.displayName = 'Toast';
+Toast.propTypes = {
+  kind: PropTypes.string,
+};
+Toast.defaultProps = {
+  kind: 'primary',
+};
+
+
+export default Toast;
