67aa7b3447c4e212eab31373669e1cbc36676518
Michael Hsu <evenchange4@gmail.com>
2017-02-22 09:21:38 +0800

    chore(circleci): try to fix testing timeout

--- tasks/test.sh
+++ tasks/test.sh
@@ -8,7 +8,7 @@ npm run stylelint
 
 ## Interface for each project
 node_modules/.bin/lerna run lint
-node_modules/.bin/lerna run test
+node_modules/.bin/lerna run --concurrency 1 test
 
 ## Test for demo page building
 node_modules/.bin/lerna run build-storybook --scope mcs-lite-ui
