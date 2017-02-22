8051b5a447896fb5c7dba3bec5036bcf194f5e6c
Michael Hsu <evenchange4@gmail.com>
2017-02-22 21:23:51 +0800

    update i18n

--- packages/mcs-lite-mobile-web/src/containers/DeviceDataChannelDetail/messages.js
+++ packages/mcs-lite-mobile-web/src/containers/DeviceDataChannelDetail/messages.js
@@ -1,16 +1,6 @@
 import { defineMessages } from 'react-intl';
 
 const messages = defineMessages({
-  'DeviceDataChannelDetail.deviceIntro': {
-    id: 'DeviceDataChannelDetail.deviceIntro',
-    description: 'dropdown',
-    defaultMessage: '測試裝置介紹',
-  },
-  'DeviceDataChannelDetail.triggerAndAction': {
-    id: 'DeviceDataChannelDetail.triggerAndAction',
-    description: 'dropdown',
-    defaultMessage: '觸發條件與動作',
-  },
 });
 
 export default messages;
