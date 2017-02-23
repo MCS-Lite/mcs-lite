31563e69bf736f0be35e99c0ac4223636d240d22
Michael Hsu <evenchange4@gmail.com>
2017-02-23 21:46:03 +0800

    feat(dangerJS): introduce danger js

---
+++ dangerfile.js
@@ -0,0 +1,6 @@
+import { danger, fail, warn } from 'danger';
+import fs from 'fs';
+
+// Make sure there are changelog entries
+const hasChangelog = danger.git.modified_files.includes('changelog.md');
+if (!hasChangelog) { fail('No Changelog changes!'); }
