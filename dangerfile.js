1cb7b4589f21930e02c04c872389edd331895b52
Michael Hsu <evenchange4@gmail.com>
2017-02-23 23:07:38 +0800

    pot checker

--- dangerfile.js
+++ dangerfile.js
@@ -1,10 +1,18 @@
-const { danger, warn } = require('danger');
+const { danger, warn, fail } = require('danger');
 // import fs from 'fs';
 
 // Make sure there are changelog entries
 // const hasChangelog = danger.git.modified_files.includes('changelog.md');
 // if (!hasChangelog) { fail('No Changelog changes!'); }
 
+// Warns if there are changes to package.json without changes to Licenses.csv.
+const i18nChanged = danger.git.modified_files.includes('messages.js');
+const potChanged = danger.git.modified_files.includes('.pot');
+if (i18nChanged && !potChanged) {
+  const message = 'Changes were made to i18n, but not to .pot';
+  const idea = 'Perhaps you need to run `$ npm run extract:pot`?';
+  fail(`${message} - <i>${idea}</i>`);
+}
 
 // Warns if there are changes to package.json without changes to Licenses.csv.
 const packageChanged = danger.git.modified_files.includes('package.json');
