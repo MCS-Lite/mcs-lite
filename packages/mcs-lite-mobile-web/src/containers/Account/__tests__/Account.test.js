c657b54e7d0a7e8aa1ff47f26ccc2ace65812756
Michael Hsu <evenchange4@gmail.com>
2017-02-24 11:23:01 +0800

    test(Signout): test for clicking

--- packages/mcs-lite-mobile-web/src/containers/Account/__tests__/Account.test.js
+++ packages/mcs-lite-mobile-web/src/containers/Account/__tests__/Account.test.js
@@ -3,6 +3,7 @@ import R from 'ramda';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import Account from '../Account';
+import { FlatButton } from '../styled-components';
 
 it('should return messages', () => {
   expect(require('../messages').default).toMatchSnapshot();
@@ -18,4 +19,22 @@ it('should renders <Account> correctly', () => {
   );
 
   expect(toJson(wrapper)).toMatchSnapshot();
+});
+
+it('should return correctly payload when clicking signout', () => {
+  const signoutMock = jest.fn();
+  const wrapper = shallow(
+    <Account
+      userName="userName"
+      signout={signoutMock}
+      getMessages={R.identity}
+    />,
+  );
+
+  // Before clicking
+  expect(signoutMock).not.toHaveBeenCalled();
+
+  // After clicking
+  wrapper.find(FlatButton).last().simulate('click');
+  expect(signoutMock).toHaveBeenCalled();
 });
