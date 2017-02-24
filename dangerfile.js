d64eb5c8153040b1c1bf71b417e53265a702376a
Michael Hsu <evenchange4@gmail.com>
2017-02-24 10:07:31 +0800

    chore(dangerJS): update rules for pot checker

--- dangerfile.js
+++ dangerfile.js
@@ -1,5 +1,11 @@
 const { danger, warn, fail } = require('danger');
 
+const allChanges = [].concat(
+  danger.git.created_files,
+  danger.git.modified_files,
+  danger.git.deleted_files,
+);
+
 // Warn if there is no description.
 if (!danger.github.pr.body.length) {
   warn('Please add a description to your PR.');
@@ -18,8 +24,8 @@ if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
 }
 
 // Warn if there are changes to i18n without extracting to pot.
-const i18nChanged = danger.git.modified_files.includes('messages.js');
-const potChanged = danger.git.modified_files.includes('.pot');
+const i18nChanged = allChanges.some(e => /messages\.js/.test(e));
+const potChanged = allChanges.some(e => /\.pot/.test(e));
 if (i18nChanged && !potChanged) {
   const message = 'Changes were made to i18n, but not to .pot';
   const idea = 'Perhaps you need to run `$ npm run extract:pot`?';
@@ -27,7 +33,7 @@ if (i18nChanged && !potChanged) {
 }
 
 // Fail if there are changes to package.json without changes to Licenses.csv.
-const packageChanged = danger.git.modified_files.includes('package.json');
+const packageChanged = allChanges.some(e => /package\.json/.test(e));
 const licensesCSVChanged = danger.git.modified_files.includes('docs/licenses.csv');
 if (packageChanged && !licensesCSVChanged) {
   const message = 'Changes were made to package.json, but not to licenses.csv';
