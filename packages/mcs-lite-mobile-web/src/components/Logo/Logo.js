98b2d8beb1c1fb7cf6bb9f7e671be9c8ac732cc9
Michael Hsu <evenchange4@gmail.com>
2017-03-03 17:22:09 +0800

    chore(LOGO): add logo images to mcs-lite-design (#171)

--- packages/mcs-lite-mobile-web/src/components/Logo/Logo.js
+++ packages/mcs-lite-mobile-web/src/components/Logo/Logo.js
@@ -1,10 +1,11 @@
 import { PropTypes } from 'react';
 import styled from 'styled-components';
-import logoPath from '../../statics/images/logo.svg';
+import logoPath from '../../statics/images/logo_mcs_lite_black.svg';
 
 const Logo = styled.img`
   margin: 16px;
   user-select: none;
+  height: 50px;
 `;
 
 Logo.displayName = 'Logo';
