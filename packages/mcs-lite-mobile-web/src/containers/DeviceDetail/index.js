11d73ef069fce15ea1d15312d632b82423d4cef3
Michael Hsu <evenchange4@gmail.com>
2017-03-03 21:04:02 +0800

    feat(mcs-lite-connect): extract web-socket hoc (#174)
    * feat(mcs-lite-connect): extract web-socket hoc

--- packages/mcs-lite-mobile-web/src/containers/DeviceDetail/index.js
+++ packages/mcs-lite-mobile-web/src/containers/DeviceDetail/index.js
@@ -3,10 +3,10 @@
 import { connect } from 'react-redux';
 import compose from 'recompose/compose';
 import { withGetMessages } from 'react-intl-inject-hoc';
+import { connectSocket } from 'mcs-lite-connect';
 import messages from './messages';
 import { actions } from '../../modules/devices';
 import DeviceDetail from './DeviceDetail';
-import connectMCS from '../../utils/connectMCS';
 
 export const mapStateToProps = ({ devices, ui }, { params: { deviceId }}) => ({
   deviceId,
@@ -22,7 +22,7 @@ const wsHost = `ws://${window.location.hostname}:${process.env.REACT_APP_SOCKET_
 
 export default compose(
   connect(mapStateToProps, mapDispatchToProps),
-  connectMCS(
+  connectSocket(
     ({ device }) => device && `${wsHost}/deviceId/${device.deviceId}/deviceKey/${device.deviceKey}`,
     props => datapoint => props.setDatapoint(props.deviceId, datapoint),
     'sendMessage', // propsName
