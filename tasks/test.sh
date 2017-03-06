09a80807b0878d87f7b39ce58de7a2c8df5ba26d
Michael Hsu <evenchange4@gmail.com>
2017-03-06 11:48:13 +0800

    try concurrency 1

--- tasks/test.sh
+++ tasks/test.sh
@@ -3,16 +3,16 @@
 set -e
 
 # global linting
-echo '💪 Start global linting';
+echo '💪  Start global linting';
 npm run eslint
 npm run stylelint
 
 ## Interface for each project
-echo '💪 Start testing for each packages';
-node_modules/.bin/lerna run lint
-node_modules/.bin/lerna run test
+echo '💪  Start testing for each packages';
+node_modules/.bin/lerna run lint --concurrency 1
+node_modules/.bin/lerna run test --concurrency 1
 
-echo '💪 Start testing for building demo page';
+echo '💪  Start testing for building demo page';
 ## Test for building demo page
 node_modules/.bin/lerna run build-storybook --scope mcs-lite-ui
 node_modules/.bin/lerna run build --scope mcs-lite-mobile-web
