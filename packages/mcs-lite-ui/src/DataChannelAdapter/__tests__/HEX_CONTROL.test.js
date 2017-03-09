7a517df61da1d6e626871822b1300765d93f9d9a
Michael Hsu <evenchange4@gmail.com>
2017-03-09 14:40:04 +0800

    HEX

---
+++ packages/mcs-lite-ui/src/DataChannelAdapter/__tests__/HEX_CONTROL.test.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'mcs-lite-theme';
+import DataChannelAdapter from '../DataChannelAdapter';
+
+describe('DataChannelAdapter', () => {
+  it('should render HEX_CONTROL correctly with default value to empty', () => {
+    const wrapper = mount(
+      <ThemeProvider theme={theme}>
+        <DataChannelAdapter
+          dataChannelProps={{
+            id: 'id',
+            type: 'HEX_CONTROL',
+            values: {},
+            format: {},
+          }}
+          eventHandler={() => {}}
+        />
+      </ThemeProvider>,
+    );
+
+    expect(wrapper.find(DataChannelAdapter)).toMatchSnapshot();
+  });
+
+  it('should render HEX_CONTROL correctly with value', () => {
+    const wrapper = mount(
+      <ThemeProvider theme={theme}>
+        <DataChannelAdapter
+          dataChannelProps={{
+            id: 'id',
+            type: 'HEX_CONTROL',
+            values: { value: 'michaelhsu' },
+            format: {},
+          }}
+          eventHandler={() => {}}
+        />
+      </ThemeProvider>,
+    );
+
+    expect(wrapper.find(DataChannelAdapter)).toMatchSnapshot();
+  });
+
+  it('should render HEX_CONTROL correctly with empty string', () => {
+    const wrapper = mount(
+      <ThemeProvider theme={theme}>
+        <DataChannelAdapter
+          dataChannelProps={{
+            id: 'id',
+            type: 'HEX_CONTROL',
+            values: { value: '' },
+            format: {},
+          }}
+          eventHandler={() => {}}
+        />
+      </ThemeProvider>,
+    );
+
+    expect(wrapper.find(DataChannelAdapter)).toMatchSnapshot();
+  });
+});
