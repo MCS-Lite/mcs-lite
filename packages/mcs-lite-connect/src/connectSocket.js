d430fcccb6ec2b20bf368497bf2187a46daa14ec
Michael Hsu <evenchange4@gmail.com>
2017-03-11 22:32:58 +0800

    feat(mcs-lite-connect): default with emptyFunction #173

--- packages/mcs-lite-connect/src/connectSocket.js
+++ packages/mcs-lite-connect/src/connectSocket.js
@@ -5,6 +5,10 @@ import createEagerFactory from 'recompose/createEagerFactory';
 import createHelper from 'recompose/createHelper';
 import { w3cwebsocket as W3CWebSocket } from 'websocket';
 
+const emptyFunction = () => {
+  console.warn('[mcs-lite-connect] W3CWebSocket is not ready.');
+};
+
 /**
  * connectSocket
  *   urlMapper => props => string
@@ -18,7 +22,7 @@ const connectSocket = (urlMapper, onMessage, sendPropsName) => (BaseComponent) =
   const factory = createEagerFactory(BaseComponent);
 
   return class ConnectMCS extends React.Component {
-    state = { exposeSenderFunction: null };
+    state = { exposeSenderFunction: emptyFunction };
     componentWillMount = () => this.createWebSocket();
     componentWillReceiveProps = () => this.createWebSocket();
     componentWillUnmount = () => this.close();
@@ -29,9 +33,9 @@ const connectSocket = (urlMapper, onMessage, sendPropsName) => (BaseComponent) =
 
       if (!this.viewer) {
         this.viewer = new W3CWebSocket(`${URL}/viewer`);
-        this.viewer.onopen = data => console.info('viewer onopen', data);
-        this.viewer.onerror = error => console.info('viewer onerror', error);
-        this.viewer.onclose = data => console.info('viewer onclose', data);
+        // this.viewer.onopen = data => console.info('viewer onopen', data);
+        // this.viewer.onerror = error => console.info('viewer onerror', error);
+        // this.viewer.onclose = data => console.info('viewer onclose', data);
         this.viewer.onmessage = (payload) => {
           const data = JSON.parse(payload.data);
           onMessage(this.props)(data); // Remind: Handle receieve messages.
@@ -42,16 +46,16 @@ const connectSocket = (urlMapper, onMessage, sendPropsName) => (BaseComponent) =
         this.sender = new W3CWebSocket(`${URL}`);
         this.sender.onopen = () =>
           this.setState({ exposeSenderFunction: this.sender.send.bind(this.sender) });
-        this.sender.onerror = error => console.info('sender onerror', error);
-        this.sender.onclose = data => console.info('sender onclose', data);
-        this.sender.onmessage = e => console.info('sender onmessage', e.data);
+        // this.sender.onerror = error => console.info('sender onerror', error);
+        // this.sender.onclose = data => console.info('sender onclose', data);
+        // this.sender.onmessage = e => console.info('sender onmessage', e.data);
       }
     }
 
     close = () => {
       this.viewer.close();
       this.sender.close();
-      this.setState({ exposeSenderFunction: null });
+      this.setState({ exposeSenderFunction: emptyFunction });
     }
 
     render() {
