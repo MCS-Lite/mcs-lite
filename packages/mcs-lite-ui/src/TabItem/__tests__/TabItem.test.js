9502d89c2ed19922b197d35bc1939203bdc8f807
Michael Hsu <evenchange4@gmail.com>
2017-03-16 10:19:49 +0800

    feat(TabItem): add new component (#230)
    * feat(TabItem): add new component

---
+++ packages/mcs-lite-ui/src/TabItem/__tests__/TabItem.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'mcs-lite-theme';
+import TabItem, { Item } from '../TabItem';
+
+it('should handle onClick', () => {
+  const mockOnClick = jest.fn();
+  const wrapper = mount(
+    <ThemeProvider theme={theme}>
+      <TabItem
+        value={123}
+        onClick={mockOnClick}
+      >
+        Tab
+      </TabItem>
+    </ThemeProvider>,
+  );
+
+  expect(mockOnClick).not.toHaveBeenCalled();
+
+  // After clicking
+  wrapper.find(Item).props().onClick('e');
+  expect(mockOnClick).toHaveBeenCalledWith('e', 123);
+});
