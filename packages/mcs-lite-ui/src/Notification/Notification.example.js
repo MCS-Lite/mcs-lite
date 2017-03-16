22447fddb39a4ea22db8d751412de2a47e1c17e5
Michael Hsu <evenchange4@gmail.com>
2017-03-16 17:27:18 +0800

    feat(Notification): add new component & Update Toast style (#235)
    * feat(Notification): add new component & Update Toast style

---
+++ packages/mcs-lite-ui/src/Notification/Notification.example.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { storiesOf } from '@kadira/storybook';
+import Notification from '.';
+import P from '../P';
+import Button from '../Button';
+
+storiesOf('Notification', module)
+  .addWithInfo(
+    'API',
+    '',
+    () =>
+      <Notification>
+        Do you want to go to Enalish version website?
+      </Notification>,
+    { inline: true },
+  )
+  .addWithInfo(
+    'With feedbck Button component',
+    '',
+    () =>
+      <Notification>
+        <P>Do you want to go to Enalish version website?</P>
+        <Button>Yes, please</Button>
+      </Notification>,
+    { inline: true },
+  );
---
+++ packages/mcs-lite-ui/src/Notification/Notification.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+import { opacity } from 'mcs-lite-theme';
+
+const lighten = opacity(0.9);
+
+const Notification = styled.div`
+  box-sizing: border-box;
+  box-shadow: 0 1px 5px 0 rgba(0, 0, 0, 0.2);
+  border-radius: 3px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 20px;
+  background-color: ${props => lighten(props.theme.color.black)};
+  color: ${props => props.theme.color.white};
+  font-size: ${props => props.theme.fontSize.p};
+
+  > *:nth-child(2) {
+    margin-left: 8px;
+    flex-shrink: 0;
+  }
+`;
+
+Notification.displayName = 'Notification';
+
+export default Notification;
