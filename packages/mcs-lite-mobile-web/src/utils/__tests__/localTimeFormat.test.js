5b9d3a167dbbb817486982044133559970353454
Michael Hsu <evenchange4@gmail.com>
2017-03-30 18:55:23 +0800

    test(mobile): use date-fns env

--- packages/mcs-lite-mobile-web/src/utils/__tests__/localTimeFormat.test.js
+++ packages/mcs-lite-mobile-web/src/utils/__tests__/localTimeFormat.test.js
@@ -1,11 +1,5 @@
 import localTimeFormat from '../localTimeFormat';
 
-// Remind: We just test the two argument of date-fns' format function.
-jest.mock('date-fns/format', () => (date, format) => ({
-  date: date.toISOString(),
-  format,
-}));
-
 it('should return localTimeFormat function', () => {
   expect(typeof localTimeFormat).toBe('function');
 });
@@ -13,5 +7,5 @@ it('should return localTimeFormat function', () => {
 it('should return correct formated string', () => {
   const fixedDate = 1489455029878; // Tue Mar 14 2017 09:30:29 GMT+0800 (CST)
 
-  expect(localTimeFormat(fixedDate)).toMatchSnapshot();
+  expect(localTimeFormat(fixedDate)).toBe('2017-03-14 09:30');
 });
