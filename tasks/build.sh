e60b29fe43388c1f6e9ae9265a534388fc198424
Michael Hsu <evenchange4@gmail.com>
2017-04-07 14:24:03 +0800

    chrore(yarn): upgrade to 0.22

--- tasks/build.sh
+++ tasks/build.sh
@@ -3,13 +3,15 @@
 set -e
 
 # MUST be in order:
-# scripts => design => icon => theme => ui
 
-node_modules/.bin/lerna run build --scope mcs-lite-scripts
-node_modules/.bin/lerna run build --scope mcs-lite-design
-node_modules/.bin/lerna run build --scope mcs-lite-icon
-node_modules/.bin/lerna run build --scope mcs-lite-theme
-node_modules/.bin/lerna run build --scope mcs-lite-ui
-node_modules/.bin/lerna run build --scope react-intl-inject-hoc
-node_modules/.bin/lerna run build --scope mcs-lite-connect
-node_modules/.bin/lerna run build --scope mcs-lite-introduction
+node_modules/.bin/lerna run build --scope mcs-lite-scripts &
+node_modules/.bin/lerna run build --scope mcs-lite-design &
+wait
+
+node_modules/.bin/lerna run build --scope mcs-lite-icon &
+node_modules/.bin/lerna run build --scope mcs-lite-theme &
+node_modules/.bin/lerna run build --scope mcs-lite-ui &
+node_modules/.bin/lerna run build --scope react-intl-inject-hoc &
+node_modules/.bin/lerna run build --scope mcs-lite-connect &
+node_modules/.bin/lerna run build --scope mcs-lite-introduction &
+wait
--- tasks/test.sh
+++ tasks/test.sh
@@ -3,14 +3,17 @@
 set -e
 
 # global linting
-yarn run eslint
-yarn run stylelint
+yarn run eslint &
+yarn run stylelint &
+wait
 
 ## Interface for each project
-node_modules/.bin/lerna run lint --concurrency 1
-node_modules/.bin/lerna run test --concurrency 1
+node_modules/.bin/lerna run lint --concurrency 1 &
+node_modules/.bin/lerna run test --concurrency 1 &
+wait
 
 echo '💪  Start testing for building demo page';
-node_modules/.bin/lerna run build-storybook --scope mcs-lite-ui
-node_modules/.bin/lerna run build --scope mcs-lite-mobile-web
-node_modules/.bin/lerna run build --scope mcs-lite-introduction
+node_modules/.bin/lerna run build-storybook --scope mcs-lite-ui &
+node_modules/.bin/lerna run build --scope mcs-lite-mobile-web &
+node_modules/.bin/lerna run build --scope mcs-lite-introduction &
+wait
