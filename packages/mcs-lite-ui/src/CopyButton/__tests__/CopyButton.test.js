fb1450b42a12a8ef37954a6a45dd81e9f4fa076c
Michael Hsu <evenchange4@gmail.com>
2017-04-11 15:03:30 +0800

    temp

--- packages/mcs-lite-ui/src/CopyButton/__tests__/CopyButton.test.js
+++ packages/mcs-lite-ui/src/CopyButton/__tests__/CopyButton.test.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import Rx from 'rxjs';
+import R from 'ramda';
 import { mount } from 'enzyme';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'mcs-lite-theme';
 import { IconLoading, IconDone } from 'mcs-lite-icon';
-import CopyButton from '../CopyButton';
+import CopyButton, { getStatusStream } from '../CopyButton';
 
 jest.mock('copy-to-clipboard');
 jest.mock('mcs-lite-icon');
 jest.unmock('react-dom');
+
+const jestRxAssert = (actual, expected) => {
+  const isEqual = R.equals(actual, expected);
+  if (!isEqual) {
+    console.error('Actual:', actual, '\n\n', 'Expected:', expected); // eslint-disable-line
+  }
+  expect(isEqual).toBe(true);
+};
 
 it('should handle onClick', (done) => {
   const wrapper = mount(
@@ -26,7 +36,22 @@ it('should handle onClick', (done) => {
   expect(wrapper.find(CopyButton).contains(<IconLoading />)).toBe(true);
 
   setTimeout(() => {
+    console.log('timeout', wrapper.find(CopyButton).contains(<IconDone />))
     expect(wrapper.find(CopyButton).contains(<IconDone />)).toBe(true);
     done();
   }, 600);
 });
+//
+// it('should return correct marble diagram with getStatusStream', () => {
+//   const scheduler = new Rx.TestScheduler(jestRxAssert);
+//   const click$ = scheduler.createHotObservable('---x');
+//   const source = getStatusStream(click$, 30, 50, scheduler);
+//   const values = {
+//     1: 'default',
+//     2: 'loading',
+//     3: 'success',
+//     4: 'default',
+//   };
+//   scheduler.expectObservable(source).toBe('1--2--3-4', values);
+//   scheduler.flush();
+// });
