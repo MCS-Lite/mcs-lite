0152f4ceea0ad815c56730795a38db77230e9ca6
Michael Hsu (徐承志) <evenchange4@gmail.com>
2017-05-11 15:21:18 +0800

    feat(storybook): add option add-on.

--- packages/mcs-lite-ui/.storybook/config.js
+++ packages/mcs-lite-ui/.storybook/config.js
@@ -1,11 +1,22 @@
 import { configure, addDecorator, setAddon } from '@kadira/storybook';
 import infoAddon from '@kadira/react-storybook-addon-info';
+import { setOptions } from '@kadira/storybook-addon-options';
 import centered from './decorator-centered';
 
 const context = require.context('../src/', true, /\.example\.js$/);
 
 setAddon(infoAddon);
 addDecorator(centered);
+setOptions({
+  name: 'MCS Lite',
+  url: 'https://github.com/MCS-Lite/mcs-lite',
+  goFullScreen: false,
+  showLeftPanel: true,
+  showDownPanel: false,
+  showSearchBox: false,
+  downPanelInRight: false,
+  sortStoriesByKind: true,
+});
 
 function loadStories() {
   context.keys().forEach(context);
