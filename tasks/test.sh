4b35688dfa5f1499692b68daf3e8b0052982dfeb
Michael Hsu (徐承志) <evenchange4@gmail.com>
2017-05-23 09:50:37 +0800

    chore(CI): update scripts

--- tasks/test.sh
+++ tasks/test.sh
@@ -14,8 +14,10 @@ node_modules/.bin/lerna run test --concurrency 1
 
 ## Test for building demo page
 echo '💪  Start testing for building demo page';
-node_modules/.bin/lerna run build-storybook --scope mcs-lite-ui
-node_modules/.bin/lerna run build --scope mcs-lite-mobile-web
 
-# Duplicate at postinstall stage
+# Duplicate at Netlify CI
+# node_modules/.bin/lerna run build-storybook --scope mcs-lite-ui
+# node_modules/.bin/lerna run build --scope mcs-lite-mobile-web
+
+# Duplicate at 'npm run postinstall' stage
 # node_modules/.bin/lerna run build --scope mcs-lite-introduction
