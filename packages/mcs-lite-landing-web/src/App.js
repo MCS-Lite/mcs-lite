15c4039314f101e24249910cdc0560f42ea5f712
Michael Hsu (徐承志) <evenchange4@gmail.com>
2017-06-14 17:55:15 +0800

    add grid

--- packages/mcs-lite-landing-web/src/App.js
+++ packages/mcs-lite-landing-web/src/App.js
@@ -2,16 +2,46 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withGetMessages } from 'react-intl-inject-hoc';
 import { Link } from 'react-router';
+import { Page, Row, Column } from 'hedron';
+import Button from 'mcs-lite-ui/lib/Button';
 import messages from './messages';
 
 const App = ({ getMessages: t }) =>
-  <div>
-    <div>
-      <h2>{t('welcome')}</h2>
-    </div>
-    <Link to="/zh-TW">zh-TW</Link> |
-    <Link to="/en">en</Link>
-  </div>;
+  <Page width="1200px">
+    <Row>
+      <Column sm={8} smShift={2} lg={6} lgShift={3}>
+        <h1>This is a column that's centered using the shift props</h1>
+
+        <div>
+          <div>
+            <h2>{t('welcome')}</h2>
+          </div>
+          <Link to="/zh-TW">zh-TW</Link> |
+          <Link to="/en">en</Link>
+        </div>
+      </Column>
+    </Row>
+    <Row>
+      <Column fluid sm={4}>
+        <h1>Fluid Columns</h1>
+        <Button>Button</Button>
+      </Column>
+      <Column fluid sm={4}>
+        <p>It's a 12 column layout by default.</p>
+      </Column>
+      <Column fluid sm={4}>
+        <Row divisions={24}>
+          <Column sm={24}>
+            <p>
+              But you can change the amount of columns by adding the
+              `divisions` property to a Row.
+            </p>
+            <p>It's fully embeddable as well!</p>
+          </Column>
+        </Row>
+      </Column>
+    </Row>
+  </Page>;
 
 App.displayName = 'App';
 App.propTypes = {
--- packages/mcs-lite-landing-web/src/index.js
+++ packages/mcs-lite-landing-web/src/index.js
@@ -5,20 +5,30 @@ import React from 'react';
 import { render } from 'react-snapshot';
 import { Router, Route, useRouterHistory, IndexRedirect } from 'react-router';
 import { createHistory } from 'history';
+import browserLocale from 'browser-locale';
+import { ThemeProvider } from 'styled-components';
+import { BreakpointProvider } from 'hedron';
+import { theme } from 'mcs-lite-theme';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import IntlProvider from './containers/IntlProvider';
+import './style';
 import './utils/i18n';
 
 const history = useRouterHistory(createHistory)();
+const DEFAULT_LOCALE = browserLocale() || 'en';
 
 render(
-  <Router history={history}>
-    <Route path="/" component={IntlProvider}>
-      <IndexRedirect to="en" />
-      <Route path=":locale" component={App} />
-    </Route>
-  </Router>,
+  <ThemeProvider theme={theme}>
+    <BreakpointProvider breakpoints={{ sm: 300, md: 568, lg: 1200 }}>
+      <Router history={history}>
+        <Route path="/" component={IntlProvider}>
+          <IndexRedirect to={DEFAULT_LOCALE} />
+          <Route path=":locale" component={App} />
+        </Route>
+      </Router>
+    </BreakpointProvider>
+  </ThemeProvider>,
   document.getElementById('root'),
 );
 registerServiceWorker();
