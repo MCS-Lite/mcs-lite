f027cfa5b71f9edcde8a8a6f8e6a3c515f8ba0c8
Michael Hsu (徐承志) <evenchange4@gmail.com>
2017-06-20 16:47:03 +0800

    raf temp

--- packages/mcs-lite-landing-web/src/containers/Header/Header.js
+++ packages/mcs-lite-landing-web/src/containers/Header/Header.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router';
 import { MenuItem } from 'mcs-lite-ui/lib/Menu';
 import Overlay from 'mcs-lite-ui/lib/Overlay';
+import raf from 'raf';
 import { Page, Row } from 'hedron';
 import { PAGE_WIDTH } from '../../components/SectionRow/SectionRow';
 import LOGO from '../../statics/images/logo_mcs_lite_black.svg';
@@ -28,6 +29,94 @@ import {
 
 const defaultLocaleMapper = localeMapper(DEFAULT_LOCALE);
 
+// Hint: Extract contenct as PureComponent to avoid re-render while scrolling
+class PurePage extends React.PureComponent {
+  static propTypes = {
+    // props
+    getMessages: PropTypes.func.isRequired,
+    router: PropTypes.object.isRequired,
+  };
+
+  state = { isShow: false, target: undefined };
+
+  onClickLanguage = () => this.setState({ isShow: !this.state.isShow });
+  onHide = () => this.setState({ isShow: false });
+  getTarget = node => this.setState({ target: node });
+
+  render() {
+    const { router, getMessages: t } = this.props;
+    const { onClickLanguage, onHide, getTarget } = this;
+    const { isShow, target } = this.state;
+
+    // Remind: fix for netlify redirect to lower case path
+    const locale = defaultLocaleMapper(router.params.locale);
+    const mcsLink = getMCSLinkByLocale(locale);
+
+    return (
+      <Page width={`${PAGE_WIDTH}px`}>
+        <Row>
+          <StyledColumn xs={12}>
+            <div>
+              <StyledA><img src={LOGO} alt="LOGO" /></StyledA>
+            </div>
+
+            <Right>
+              <StyledA href={mcsLink} target="_blank" rel="noreferrer noopener">
+                {t('gotoMCS')}
+              </StyledA>
+              <StyledA
+                href={`https://mcs-lite-introduction.netlify.com/${locale}`}
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                {t('resource')}
+              </StyledA>
+              <StyledA
+                href="mailto:mtkcloudsandbox@mediatek.com"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                {t('contact')}
+              </StyledA>
+              <StyledA
+                ref={getTarget}
+                onClick={onClickLanguage}
+                data-ga-on="click"
+                data-ga-event-category="Language Dropdown Menu"
+                data-ga-event-action="click"
+              >
+                <StyledP>Language</StyledP>
+                <StyledIconFold size={16} isShow={isShow} />
+              </StyledA>
+
+              {isShow &&
+                <Overlay target={target} onClickOutSide={onHide}>
+                  <StyledMenu key="menu">
+                    {LOCALES.map(({ id, children }) =>
+                      <Link key={id} to={`/${id}`} onClick={onHide}>
+                        <MenuItem>{children}</MenuItem>
+                      </Link>,
+                    )}
+                  </StyledMenu>
+                </Overlay>}
+            </Right>
+
+            {/* For Prereder */}
+            <HiddenForPreRenderTrick>
+              {LOCALES.map(({ id, children }) =>
+                <Link key={id} to={`/${id}`}>
+                  {children}
+                </Link>,
+              )}
+            </HiddenForPreRenderTrick>
+
+          </StyledColumn>
+        </Row>
+      </Page>
+    );
+  }
+}
+
 class Header extends React.Component {
   static propTypes = {
     // React-intl I18n
@@ -37,91 +126,27 @@ class Header extends React.Component {
     router: PropTypes.object.isRequired,
   };
 
-  state = { isTop: true, isShow: false, target: undefined };
+  state = { isTop: true };
+
   componentDidMount = () => window.addEventListener('scroll', this.onScroll);
-  componentWillUnmount = () =>
+  componentWillUnmount = () => {
     window.removeEventListener('scroll', this.onScroll);
-  onScroll = () => this.setState({ isTop: document.body.scrollTop < 40 });
-  onClickLanguage = () => this.setState({ isShow: !this.state.isShow });
-  onHide = () => this.setState({ isShow: false });
-  getTarget = node => this.setState({ target: node });
+    raf.cancel(this.rafId);
+  };
+  onScroll = () => {
+    this.rafId = raf(() => {
+      this.setState({ isTop: document.body.scrollTop < 40 });
+    });
+  };
 
   render() {
-    const { router, getMessages: t } = this.props;
-    const { onClickLanguage, onHide, getTarget } = this;
-    const { isShow, target, isTop } = this.state;
-
-    // Remind: fix for netlify redirect to lower case path
-    const locale = defaultLocaleMapper(router.params.locale);
-    const mcsLink = getMCSLinkByLocale(locale);
+    const { router, getMessages } = this.props;
+    const { isTop } = this.state;
 
     return (
       <Container>
         <Fixed isTop={isTop}>
-          <Page width={`${PAGE_WIDTH}px`}>
-            <Row>
-              <StyledColumn xs={12}>
-                <div>
-                  <StyledA><img src={LOGO} alt="LOGO" /></StyledA>
-                </div>
-
-                <Right>
-                  <StyledA
-                    href={mcsLink}
-                    target="_blank"
-                    rel="noreferrer noopener"
-                  >
-                    {t('gotoMCS')}
-                  </StyledA>
-                  <StyledA
-                    href={`https://mcs-lite-introduction.netlify.com/${locale}`}
-                    target="_blank"
-                    rel="noreferrer noopener"
-                  >
-                    {t('resource')}
-                  </StyledA>
-                  <StyledA
-                    href="mailto:mtkcloudsandbox@mediatek.com"
-                    target="_blank"
-                    rel="noreferrer noopener"
-                  >
-                    {t('contact')}
-                  </StyledA>
-                  <StyledA
-                    ref={getTarget}
-                    onClick={onClickLanguage}
-                    data-ga-on="click"
-                    data-ga-event-category="Language Dropdown Menu"
-                    data-ga-event-action="click"
-                  >
-                    <StyledP>Language</StyledP>
-                    <StyledIconFold size={18} isShow={isShow} />
-                  </StyledA>
-
-                  {isShow &&
-                    <Overlay target={target} onClickOutSide={onHide}>
-                      <StyledMenu key="menu">
-                        {LOCALES.map(({ id, children }) =>
-                          <Link key={id} to={`/${id}`} onClick={onHide}>
-                            <MenuItem>{children}</MenuItem>
-                          </Link>,
-                        )}
-                      </StyledMenu>
-                    </Overlay>}
-                </Right>
-
-                {/* For Prereder */}
-                <HiddenForPreRenderTrick>
-                  {LOCALES.map(({ id, children }) =>
-                    <Link key={id} to={`/${id}`}>
-                      {children}
-                    </Link>,
-                  )}
-                </HiddenForPreRenderTrick>
-
-              </StyledColumn>
-            </Row>
-          </Page>
+          <PurePage router={router} getMessages={getMessages} />
         </Fixed>
       </Container>
     );
