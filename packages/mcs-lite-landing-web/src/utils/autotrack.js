c805da4fdc6a44986632a76045b2d258d0047c56
Michael Hsu <evenchange4@gmail.com>
2017-06-20 11:57:09 +0800

    fix(Landing/GA): switch from code split to CDN with async scripts in HTML (#375)

--- packages/mcs-lite-landing-web/src/utils/autotrack.js
+++ packages/mcs-lite-landing-web/src/utils/autotrack.js
@@ -1,97 +1,90 @@
 /* global ga */
 
-import { BREAKPOINTS } from './landingTheme';
-
-/**
- * Custom dimension. https://goo.gl/tp4uf9
- */
-const DIMENSION_MEDIA_QUERY = 1;
-const DIMENSION_CLEAN_URL = 2;
-
-const autotrack = id => {
+export default function autotrack(id, breakpoints) {
   if (!id) return;
 
-  // TODO: DO not import all plugins https://github.com/googleanalytics/autotrack/issues/137
-  import('autotrack/autotrack').then(() => {
-    // prettier-ignore
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga'); // eslint-disable-line
+  /**
+   * Custom dimension for projects
+   */
+  const DIMENSION_MEDIA_QUERY = 1;
+  const DIMENSION_CLEAN_URL = 2;
 
-    ga('create', id, 'auto');
+  // prettier-ignore
+  window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date; // eslint-disable-line
 
-    /**
-     * Avoiding the different urls actually point to the same page.
-     * Modifying the 'page' field.
-     */
-    ga('require', 'cleanUrlTracker', {
-      stripQuery: true,
-      queryParamsWhitelist: ['q', 'locale'],
-      queryDimensionIndex: DIMENSION_CLEAN_URL,
-      indexFilename: 'index.html',
-      trailingSlash: 'remove',
-    });
+  ga('create', id, 'auto');
 
-    /**
-     * Declarative event tracking via HTML attributes.
-     * event
-     */
-    ga('require', 'eventTracker', {
-      attributePrefix: 'data-ga-',
-    });
-
-    /**
-     * Tracks how far down the page a user scrolls.
-     * 'Max Scroll'
-     */
-    ga('require', 'maxScrollTracker');
-
-    /**
-     * Tracking media query.
-     * 'definition.name'
-     */
-    ga('require', 'mediaQueryTracker', {
-      definitions: [
-        {
-          name: 'Breakpoint',
-          dimensionIndex: DIMENSION_MEDIA_QUERY,
-          items: [
-            { name: 'xs', media: 'all' },
-            ...Object.keys(BREAKPOINTS).map(k => ({
-              name: k,
-              media: `(min-width: ${BREAKPOINTS[k]}px)`,
-            })),
-          ],
-        },
-      ],
-      changeTimeout: 3000,
-    });
-
-    /**
-     * Tracks form submits to external domains.
-     * 'Outbound Form'
-     */
-    ga('require', 'outboundFormTracker');
-
-    /**
-     * Tracks link clicks to external domains.
-     * 'Outbound Link'
-     */
-    ga('require', 'outboundLinkTracker');
-
-    /**
-     * Capturing how long the page was in the visible state.
-     * Note: It send the initial pageview.
-     * 'pageview' | 'Page Visibility' | 'page load'
-     */
-    ga('require', 'pageVisibilityTracker', {
-      sendInitialPageview: true,
-    });
-
-    /**
-     * URL changes for SPA.
-     * 'pageview'
-     */
-    ga('require', 'urlChangeTracker');
+  /**
+   * Avoiding the different urls actually point to the same page.
+   * Modifying the 'page' field.
+   */
+  ga('require', 'cleanUrlTracker', {
+    stripQuery: true,
+    queryParamsWhitelist: ['q', 'locale'],
+    queryDimensionIndex: DIMENSION_CLEAN_URL,
+    indexFilename: 'index.html',
+    trailingSlash: 'remove',
   });
-};
 
-export default autotrack;
+  /**
+   * Declarative event tracking via HTML attributes.
+   * event
+   */
+  ga('require', 'eventTracker', {
+    attributePrefix: 'data-ga-',
+  });
+
+  /**
+   * Tracks how far down the page a user scrolls.
+   * 'Max Scroll'
+   */
+  ga('require', 'maxScrollTracker');
+
+  /**
+   * Tracking media query.
+   * 'definition.name'
+   */
+  ga('require', 'mediaQueryTracker', {
+    definitions: [
+      {
+        name: 'Breakpoint',
+        dimensionIndex: DIMENSION_MEDIA_QUERY,
+        items: [
+          { name: 'xs', media: 'all' },
+          ...Object.keys(breakpoints).map(k => ({
+            name: k,
+            media: `(min-width: ${breakpoints[k]}px)`,
+          })),
+        ],
+      },
+    ],
+    changeTimeout: 3000,
+  });
+
+  /**
+   * Tracks form submits to external domains.
+   * 'Outbound Form'
+   */
+  ga('require', 'outboundFormTracker');
+
+  /**
+   * Tracks link clicks to external domains.
+   * 'Outbound Link'
+   */
+  ga('require', 'outboundLinkTracker');
+
+  /**
+   * Capturing how long the page was in the visible state.
+   * Note: It send the initial pageview.
+   * 'pageview' | 'Page Visibility' | 'page load'
+   */
+  ga('require', 'pageVisibilityTracker', {
+    sendInitialPageview: true,
+  });
+
+  /**
+   * URL changes for SPA.
+   * 'pageview'
+   */
+  ga('require', 'urlChangeTracker');
+}
