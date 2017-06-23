728a01a914b45cfe2729c92ca4809876137a63b8
Michael Hsu (徐承志) <evenchange4@gmail.com>
2017-06-23 17:07:06 +0800

    section 5

--- packages/mcs-lite-landing-web/src/containers/Section5/Section5.js
+++ packages/mcs-lite-landing-web/src/containers/Section5/Section5.js
@@ -8,9 +8,9 @@ import TextCenter from 'mcs-lite-ui/lib/TextCenter';
 import SpaceTop from 'mcs-lite-ui/lib/SpaceTop';
 import SectionRow from '../../components/SectionRow';
 import DownloadButton from '../../components/DownloadButton';
-import imgSetup from '../../statics/images/img_setup.svg';
-import imgMachine from '../../statics/images/img_setup_machine.svg';
-import imgScreen from '../../statics/images/img_setup_screen.svg';
+import SVGBackground from './SVGBackground';
+import SVGMachine from './SVGMachine';
+import SVGScreen from './SVGScreen';
 
 const ImageWrapper = styled.div`
   position: relative;
@@ -27,13 +27,6 @@ const ScrollParallaxMachine = styled(ScrollParallax)`
   transform-origin: 143px 0;
 `;
 
-const Background = styled.div`
-  background-image: url("${props => props.src}");
-  background-repeat: no-repeat;
-  background-position: center center;
-  height: 100%;
-`;
-
 const BackgroundOverlay = styled.div`
   background: linear-gradient(90deg, rgba(255, 255, 255, 1) 2%, rgba(255, 255, 255, 0) 8%, rgba(255, 255, 255, 0) 92%, rgba(255, 255, 255, 1) 98%);
   height: 100%;
@@ -42,38 +35,41 @@ const BackgroundOverlay = styled.div`
 `;
 
 const StyledTextCenter = styled(TextCenter)`
-  overflow-x: hidden;;
+  overflow-x: hidden;
 `;
+
+const Image = () =>
+  <ImageWrapper>
+    <div>
+      <ScrollParallax
+        animation={{ x: 0, y: 0, playScale: [0, 0.4] }}
+        style={{ transform: 'translate(-5px, -32px)' }}
+        component={SVGScreen}
+      />
+    </div>
+    <div>
+      <ScrollParallaxMachine
+        animation={{ rotate: 0, playScale: [0, 0.4] }}
+        style={{ transform: 'translateX(60px) rotate(15deg)' }}
+        component={SVGMachine}
+      />
+    </div>
+    <div>
+      <SVGBackground />
+    </div>
+    <BackgroundOverlay />
+  </ImageWrapper>;
 
 const Section5 = ({ tag, getMessages: t }) =>
   <SectionRow>
     <Column xs={12}>
       <StyledTextCenter>
         <Heading level={2}>{t('title')}</Heading>
+
         <SpaceTop height={40}>
-          <ImageWrapper>
-            <div>
-              <ScrollParallax
-                animation={{ x: 0, y: 0, playScale: [0, 0.4] }}
-                style={{ transform: 'translate(-5px, -32px)' }}
-                component="img"
-                src={imgScreen}
-                alt="screen"
-              />
-            </div>
-            <div>
-              <ScrollParallaxMachine
-                animation={{ rotate: 0, playScale: [0, 0.4] }}
-                style={{ transform: 'translateX(60px) rotate(15deg)' }}
-                component="img"
-                src={imgMachine}
-                alt="machine"
-              />
-            </div>
-            <Background src={imgSetup} />
-            <BackgroundOverlay />
-          </ImageWrapper>
+          <Image />
         </SpaceTop>
+
         <SpaceTop height={40}>
           <DownloadButton tag={tag} />
         </SpaceTop>
