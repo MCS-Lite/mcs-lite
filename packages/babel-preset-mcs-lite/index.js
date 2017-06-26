22fbb259ca6141aab0ca4eafa0547784dd15950a
Michael Hsu (徐承志) <evenchange4@gmail.com>
2017-06-26 16:28:13 +0800

    mini

--- packages/babel-preset-mcs-lite/index.js
+++ packages/babel-preset-mcs-lite/index.js
@@ -26,7 +26,7 @@ module.exports = {
 
         // Only necessary if you're server-side renderin
         ssr: false,
-        minify: false, // TODO: CI snapshot testing problem
+        minify: true, // TODO: CI snapshot testing problem
         preprocess: false,
         transpileTemplateLiterals: false,
       },
