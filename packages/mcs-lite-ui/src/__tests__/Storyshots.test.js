26297e6d6de047dd38822c649074d9106dbd0e2d
Michael Hsu (徐承志) <evenchange4@gmail.com>
2017-06-29 15:28:30 +0800

    add

--- packages/mcs-lite-ui/src/__tests__/Storyshots.test.js
+++ packages/mcs-lite-ui/src/__tests__/Storyshots.test.js
@@ -1,4 +1,14 @@
 import initStoryshots from '@storybook/addon-storyshots';
+import 'jest-styled-components';
+import renderer from 'react-test-renderer';
+
+// Using this function means we'll see the change in the css in the snapshot
+// diff instead of just the change in classname
+const styledSnapshot = ({ story, context }) => {
+  const storyElement = story.render(context);
+  const tree = renderer.create(storyElement).toJSON();
+  expect(tree).toMatchStyledComponentsSnapshot();
+};
 
 /**
  * Ignore Snapshot testing with "[Skip]" in story name.
@@ -8,4 +18,5 @@ import initStoryshots from '@storybook/addon-storyshots';
 
 initStoryshots({
   storyRegex: /^((?!\[Skip\]).)*$/,
+  test: styledSnapshot,
 });
