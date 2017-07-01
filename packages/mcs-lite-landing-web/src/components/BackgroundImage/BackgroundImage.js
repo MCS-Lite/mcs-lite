1aacea6c57ca4d51c7c6c00c59d970586072ae01
Michael Hsu (徐承志) <evenchange4@gmail.com>
2017-07-01 13:12:47 +0800

    update blur

--- packages/mcs-lite-landing-web/src/components/BackgroundImage/BackgroundImage.js
+++ packages/mcs-lite-landing-web/src/components/BackgroundImage/BackgroundImage.js
@@ -1,10 +1,14 @@
+import React from 'react';
 import styled from 'styled-components';
 import ProgressiveImage from 'react-progressive-bg-image';
 
-const BackgroundImage = styled(ProgressiveImage)`
+const StyledProgressiveImage = styled(ProgressiveImage)`
   background-position: center center;
   height: 100%;
   background-size: auto 100%;
 `;
+
+const BackgroundImage = props =>
+  <StyledProgressiveImage blur={0.5} opacity={1} {...props} />;
 
 export default BackgroundImage;
