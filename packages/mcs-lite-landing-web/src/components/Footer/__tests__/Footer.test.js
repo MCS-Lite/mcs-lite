87b37f01682e7c0c1e313cc201c8c3806532c225
Michael Hsu <evenchange4@gmail.com>
2018-01-02 12:39:23 +0800

    fix(Landing/Footer): mock year 新年快樂 🎉 (#486)

--- packages/mcs-lite-landing-web/src/components/Footer/__tests__/Footer.test.js
+++ packages/mcs-lite-landing-web/src/components/Footer/__tests__/Footer.test.js
@@ -3,6 +3,8 @@ import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import Footer from '../Footer';
 
+jest.mock('mcs-lite-ui/lib/utils/getCurrentYear', () => () => 'mockYear');
+
 it('should renders <Footer> correctly', () => {
   const wrapper = shallow(<Footer getMessages={t => t} />);
 
