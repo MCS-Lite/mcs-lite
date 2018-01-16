fbd5b35db6fa11f2bee218b02f383269d76caa02
Michael Hsu (徐承志) <evenchange4@gmail.com>
2018-01-16 23:12:27 +0800

    test(mcs-lite-ui): separate storyshot files

--- packages/mcs-lite-ui/src/__tests__/Storyshots.test.js
+++ packages/mcs-lite-ui/src/__tests__/Storyshots.test.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import initStoryshots from '@storybook/addon-storyshots';
+// @flow
+import * as React from 'react';
+import initStoryshots, {
+  getSnapshotFileName,
+} from '@storybook/addon-storyshots';
 import renderer from 'react-test-renderer';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'mcs-lite-theme';
+import { mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import styleSheetSerializer from 'jest-styled-components/src/styleSheetSerializer';
+import { addSerializer } from 'jest-specific-snapshot';
 
-// Using this function means we'll see the change in the css in the snapshot
-// diff instead of just the change in classname
-const styledSnapshot = ({ story, context }) => {
-  const storyElement = (
-    <ThemeProvider theme={theme}>{story.render(context)}</ThemeProvider>
-  );
-  const tree = renderer.create(storyElement).toJSON();
-  expect(tree).toMatchSnapshot();
-};
+/**
+ * Add custom serializer for toMatchSpecificSnapshot
+ * ref: https://github.com/storybooks/storybook/issues/887#issuecomment-357073807
+ */
+addSerializer(styleSheetSerializer);
 
 /**
  * Ignore Snapshot testing with "[Skip]" in story name.
  *
  * @author Michael Hsu
  */
-
 initStoryshots({
   storyRegex: /^((?!\[Skip\]).)*$/,
-  test: styledSnapshot,
+  test: ({ story, context }) => {
+    const snapshotFileName = getSnapshotFileName(context);
+    const storyElement = (
+      <ThemeProvider theme={theme}>{story.render(context)}</ThemeProvider>
+    );
+
+    const tree = renderer.create(storyElement).toJSON();
+    // expect(tree).toMatchSnapshot();
+    (expect(tree): any).toMatchSpecificSnapshot(snapshotFileName);
+
+    // Note: We just snapshot children without the ThemeProvider
+    // const tree = mount(storyElement).children();
+    // const json = toJson(tree);
+
+    // if (snapshotFileName) {
+    //   // Remind: property `toMatchSpecificSnapshot`. Property not found in Jest flowtype
+    //   (expect(json): any).toMatchSpecificSnapshot(snapshotFileName);
+    // }
+  },
 });
