555f1597aade4120dba5c339a2fd66691f02837a
liuderchi <liuderchi@gmail.com>
2018-04-16 16:08:40 +0800

    fix(avatar): use prototype `defaultProps`, `propTypes` for consumers

--- packages/mcs-lite-ui/src/Avatar/Avatar.js
+++ packages/mcs-lite-ui/src/Avatar/Avatar.js
@@ -1,5 +1,6 @@
 // @flow
 import * as React from 'react';
+import PropTypes from 'prop-types';
 import { compose, pure } from 'recompose';
 import IconAvatar from './IconAvatar';
 import Wrapper from './styled-components';
@@ -9,15 +10,24 @@ type Props = {
   size?: number,
 };
 
-const PureAvatar = ({ src, size = 30 }: Props) => (
-  <Wrapper>
-    {src ? (
-      <img alt="" src={src} width={size} height={size} />
-    ) : (
-      <IconAvatar width={size} height={size} />
-    )}
-  </Wrapper>
-);
+function PureAvatar({ src, size }: Props) {
+  return (
+    <Wrapper>
+      {src ? (
+        <img alt="" src={src} width={size} height={size} />
+      ) : (
+        <IconAvatar width={size} height={size} />
+      )}
+    </Wrapper>
+  );
+}
+PureAvatar.defaultProps = {
+  size: '30',
+};
+PureAvatar.propTypes = {
+  src: PropTypes.string,
+  size: PropTypes.string,
+};
 
 const enhance = compose(pure);
 const Avatar: React.ComponentType<Props> = enhance(PureAvatar);
